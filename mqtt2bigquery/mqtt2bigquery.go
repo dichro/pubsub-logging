ead4ec85136879d2713c396a85dd707feccf8871
Miki Habryn <miki@rcpt.to>
2019-03-26 01:43:57 -0700

    streaming batches to bigquery

--- mqtt2bigquery/mqtt2bigquery.go
+++ mqtt2bigquery/mqtt2bigquery.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
 	"net/http"
 	"time"
 
+	"cloud.google.com/go/bigquery"
 	paho "github.com/eclipse/paho.mqtt.golang"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -16,6 +18,7 @@ var (
 	httpAddr  = flag.String("http_listen", ":8080", "address to listen on for http requests (addr:port)")
 	mqttAddr  = flag.String("mqtt_address", "tcp://mqtt:1883", "address of MQTT broker")
 	mqttTopic = flag.String("mqtt_topic", "syslog/raw/json", "MQTT topic for raw syslog messages")
+	bqProject = flag.String("bq_project", "", "GCP BigQuery project ID to stream logs to")
 )
 
 type Message struct {
@@ -44,8 +47,15 @@ func main() {
 		glog.Fatal(token.Error())
 	}
 
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, *bqProject)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	b := NewBuffer()
+	go b.Stream(ctx, client, 500, time.Minute)
 	glog.Infof("subscribing to topic %q", *mqttTopic)
-	token := mqtt.Subscribe(*mqttTopic, 1, handler)
+	token := mqtt.Subscribe(*mqttTopic, 1, b.Add)
 	token.Wait()
 	if err := token.Error(); err != nil {
 		glog.Fatal(err)
@@ -56,11 +66,54 @@ func main() {
 	glog.Fatal(http.ListenAndServe(*httpAddr, nil))
 }
 
-func handler(c paho.Client, m paho.Message) {
-	var log Message
-	if err := json.NewDecoder(bytes.NewReader(m.Payload())).Decode(&log); err != nil {
-		glog.Error(err)
+type Buffer struct {
+	ch chan Message
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{
+		ch: make(chan Message),
 	}
+}
+
+// why is this a single-threaded call, hmm?
+func (b *Buffer) Add(c paho.Client, m paho.Message) {
+	var msg Message
 	glog.V(1).Infof("received message %s", string(m.Payload()))
-	glog.V(1).Infof("extracted messages %#v", log)
+	if err := json.NewDecoder(bytes.NewReader(m.Payload())).Decode(&msg); err != nil {
+		glog.Error(err)
+		return
+	}
+	glog.V(1).Infof("extracted messages %#v", msg)
+	b.ch <- msg
+	glog.V(1).Info("done with message")
+}
+
+func (b *Buffer) Stream(ctx context.Context, bq *bigquery.Client, maxBatch int, maxDelay time.Duration) {
+	ins := bq.Dataset("logs").Table("syslog").Inserter()
+	batchArray := make([]Message, 0, maxBatch)
+	for {
+		// set up a new slice that will reuse the existing array
+		batch := batchArray[0:0]
+		// wait for first message of the batch
+		batch = append(batch, <-b.ch)
+		timeout := time.NewTicker(maxDelay)
+		for {
+			select {
+			case msg := <-b.ch:
+				batch = append(batch, msg)
+				if len(batch) < cap(batch) {
+					continue
+				}
+			case <-timeout.C:
+				// drop out
+			}
+			timeout.Stop()
+			glog.Infof("sending batch size %d of max %d", len(batch), cap(batch))
+			if err := ins.Put(ctx, batch); err != nil {
+				glog.Error(err)
+			}
+			break
+		}
+	}
 }
